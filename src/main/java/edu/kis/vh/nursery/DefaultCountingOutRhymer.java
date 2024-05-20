79d62aeef9d5d9f57e62eaf1a9f66f32ef63c9b9
Szymon Gieraga <245809@edu.p.lodz.pl>
2024-05-20 14:17:30 +0200

    6.2 Zastąpienie literałów stałymi w pliku DefaultCountingOutrhymer.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
     static final int EMPTY = -1;
-    static final int FULL = 11;
     static final int SIZE = 12;
+    static final int FULL = SIZE - 1;    
 
 
     private int[] numbers = new int[SIZE];
@@ -25,13 +25,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return numbers[total--];
     }
 
