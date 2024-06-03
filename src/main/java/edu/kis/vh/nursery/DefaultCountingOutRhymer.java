3809e233e5c950c704c13f65f66b43fdfb6fae11
Sebastian <245787@edu.p.lodz.pl>
2024-06-03 18:27:46 +0200

    17. Stworzenie dokumentacji dla klasy DefaultCountingOutRhymer.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,7 @@
 package edu.kis.vh.nursery;
-
+/*
+    The DefaultCountingOutRhymer class is used to count out rhymers.
+*/
 public class DefaultCountingOutRhymer {
     private static final int EMPTY = -1;
     private final int MAX_SIZE = 12;
@@ -9,32 +11,46 @@ public class DefaultCountingOutRhymer {
 
 
     private final int[] numbers = new int[MAX_SIZE];
-
+    /*
+        getTotal method returns value of elements in array numbers.
+        variable total is a number of rhymers in array.
+     */
     public int getTotal() {
         return total;
     }
 
     private int total = EMPTY;
-
+    /*
+        countIn method inserts value into numbers array after the last element, if array isn't full.
+        argument in is a value that will be inserted into array as the next element.
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
-
+    /*
+        callCheck is a method that returns true, if array number is empty, otherwise returns false.
+     */
     public boolean callCheck() {
         return total == DEFAULT;
     }
-
+    /*
+        isFull is a method that returns true, if array number is full.
+    */
     public boolean isFull() {
         return total == FULL;
     }
-
+    /*
+        peekaboo is a method that returns value of the last inserted element into number's array, if array isn't empty, otherwise returns DEFAULT value.
+     */
     protected int peekaboo() {
         if (callCheck())
             return DEFAULT;
         return numbers[total];
     }
-
+    /*
+        countOut method is returning last inserted elements and reduce last element, if array numbers isn't empty, otherwise returns EMPTY value.
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY;
